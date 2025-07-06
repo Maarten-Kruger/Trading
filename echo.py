e405339480af555ac388892ee28e09798226e826
Maarten Kruger <52831086@mynwu.ac.za>
2025-07-06 21:14:39 +0200

    Clearing

--- echo.py
+++
@@ -1,8 +0,0 @@
-while True:
-    try:
-        line = input()
-    except EOFError:
-        break
-    if not line:
-        break
-    print(line)
--- print_csv.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open('data.csv', newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(row)
-
